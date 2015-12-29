d31831e3f1d0116e71799fcc5c67d342749d3bd7
sadhnakaushik <sadhnasharma34@gmail.com>
2015-12-29 16:35:40 +0530

    Added page for reporting

--- application/views/User/view_status.php
+++ application/views/User/view_status.php
@@ -1,50 +1,45 @@
-     <script src="<?php echo asset_url() ?>js/highcharts.js" type="text/javascript"></script>
-   
-      
-               
-           <form class="card">
-                <ul class="table-view " >
-                    <li class="table-view-cell" style="text-align: center">
-                        <b>Jan 2016 Result</b>
-                        </li>
-               <li class="table-view-cell">
-           
-                     Sort By
+<script src="<?php echo asset_url() ?>js/highcharts.js" type="text/javascript"></script>
+
+
+
+<form class="card">
+    <ul class="table-view " >
+        <li class="table-view-cell" style="text-align: center">
+            <b>Jan 2016 Result</b>
+        </li>
+        <li class="table-view-cell">
+
+            Sort By
             <select class="form-control ">
                 <option>Please Select</option>
                 <option>Parameter1</option>
-                    <option>Parameter2</option>
-                    <option>Parameter3</option>      
-            
-                   
-            </select>
-               </li>
-                </ul>
-                    
-           
-           <div class="col-lg-12">
-                <div class="panel-body" id="piechart">
-                </div>
-                </div>
-                </form>
+                <option>Parameter2</option>
+                <option>Parameter3</option>      
 
-        
-      </div>
- 
-   
+
+            </select>
+        </li>
+        <li class="table-view-cell" id="piechart">
+        </li>
+    </ul>
+</form>
+
+
+</div>
+
+
 <script>
     $(function () {
         $('#piechart').highcharts({
             chart: {
                 plotBackgroundColor: null,
-              
                 plotShadow: false
             },
             title: {
                 text: ''
-               
-                
-                
+
+
+
             },
             tooltip: {
                 pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -61,18 +56,15 @@
                     dataLabels: {
                         enabled: true,
                         format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                       
                     }
                 }
             },
-            
             series: [{
                     type: 'pie',
                     name: 'Class',
-                   data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5]
-                    
+                    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5],
                 }]
-            
+
         });
     });
 </script>
