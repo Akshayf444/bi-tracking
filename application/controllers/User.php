3574cc82d7ec3db52ad22fa31c7be2cb715fe020
raiyan2723 <raiyanshaikh1993@gmail.com>
2016-01-05 16:40:17 +0530

    sas

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -81,6 +81,7 @@ class User extends MY_Controller {
             {
                 $data['show4'] = $this->User_model->Rx_Target_month2($this->session->userdata('VEEVA_Employee_ID'), $this->Product_Id, $current_month);
                 $data['Planned'] = $this->User_model->Planned_Rx_Count();
+                $data['Actual'] = $this->User_model->Actual_Rx_Count();
                 
             }
             $data['productList'] = $this->Master_Model->generateDropdown($result, 'id', 'Brand_Name', $this->Product_Id);
