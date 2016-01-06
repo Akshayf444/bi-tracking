4dea4223ee54206b1ee592ddfc93bdc0be7bb6b3
raiyan2723 <raiyanshaikh1993@gmail.com>
2016-01-06 17:08:26 +0530

    added pages in admin panel

--- application/views/template3.php
+++ application/views/template3.php
@@ -15,7 +15,7 @@
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
         <link href="<?php echo asset_url() ?>css/jquery-ui.css" rel="stylesheet" type="text/css"/>
-<link href="<?php echo asset_url() ?>css/bootstrap.min.css" rel="stylesheet" type="text/css " >
+        <link href="<?php echo asset_url() ?>css/bootstrap.min.css" rel="stylesheet" type="text/css " >
 
         <link href="<?php echo asset_url() ?>css/metisMenu.min.css" rel="stylesheet" type="text/css " >
 
@@ -47,39 +47,39 @@
 
     <body>
 
-        <body>
+    <body>
 
-    <div id="wrapper">
+        <div id="wrapper">
 
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+            <!-- Navigation -->
+            <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
                     <img width="160px" height="68px" class="navbar" onclick="window.location = '<?php echo site_url('admin/dashboard'); ?>';" src="<?php echo asset_url() ?>images/travels.png" alt=""/>
 
                 </div>
-             <ul class="nav navbar-top navbar-right">
-                <li class="dropdown">
-                    
-                      
-                                <a class="fa fa-2x fa-power-off pull-right" onclick="window.location = '<?php echo site_url('admin/logout'); ?>';" ></a>
-                       
-                </li>
-             </ul>
+                <ul class="nav navbar-top navbar-right">
+                    <li class="dropdown">
+
+
+                        <a class="fa fa-2x fa-power-off pull-right" onclick="window.location = '<?php echo site_url('admin/logout'); ?>';" ></a>
+
+                    </li>
+                </ul>
                 <!-- /.navbar-header -->
-<!--  <ul class="nav navbar top-nav">
-                  
-                
-                                              <a class="fa fa-2x fa-power-off pull-right" onclick="window.location = '<?php echo site_url('admin/logout'); ?>';" ></a>
-                       
-                    </ul>-->
-                    <!-- /.dropdown-user -->
-                
+                <!--  <ul class="nav navbar top-nav">
+                                  
+                                
+                                                              <a class="fa fa-2x fa-power-off pull-right" onclick="window.location = '<?php echo site_url('admin/logout'); ?>';" ></a>
+                                       
+                                    </ul>-->
+                <!-- /.dropdown-user -->
+
 
 
 
@@ -87,7 +87,7 @@
                 <!-- /.navbar-top-links -->
 
                 <div class="navbar sidebar" role="navigation" >
-                     <div class="sidebar-nav navbar-collapse">
+                    <div class="sidebar-nav navbar-collapse">
 
                         <ul class="nav" id="side-menu">
                             <li>
@@ -109,6 +109,14 @@
                                 <a  onclick="window.location = '<?php echo site_url('admin/doc_view'); ?>';" >Doctor</a>
 
                             </li>
+                            <li class="">
+                                <a   >Reports</a>
+                                <ul class="" role="menu">
+                                    <li><a onclick="window.location = '<?php echo site_url('admin/Profile_Completion'); ?>';">Profile Completion Status</a></li>
+                                    <li><a onclick="window.location = '<?php echo site_url('admin/Rx_Planning'); ?>';">Rx Planning</a></li>
+                                    <li><a href="#">Reporting For Rx</a></li>
+                                </ul>
+                            </li>
                         </ul>
 
                     </div>
@@ -116,8 +124,8 @@
                 </div>
 
                 <!-- /.navbar-static-side -->
-            
-</nav>
+
+            </nav>
 
             <div id="page-wrapper">
                 <?php $this->load->view($content, $view_data); ?>
