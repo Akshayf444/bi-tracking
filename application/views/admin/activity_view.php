3ee19ee501187cad0959480c2f96c4a069bf8138
Akshayf444 <mohite.akshay118@gmail.com>
2016-01-07 15:39:36 +0530

    Added new design for admin panel

--- application/views/admin/activity_view.php
+++ application/views/admin/activity_view.php
@@ -1,57 +1,37 @@
-<style>
-    .table-view .table-view-cell {
-        background-position: 0px 100%;
-    }
-    .col-xs-9, .col-xs-3{
-        padding: 0px;
-    }
-    .table-view-cell {
-        padding: 11px 12px 11px 15px;
-    }
-</style>
 <div class="row">
-    <div class="col-lg-12">
-        <h1 class="page-header"> Activity List </h1>
+    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+        <a class="btn btn-primary" onclick="window.location = '<?php echo site_url('admin/add_activity'); ?>';"> Add  Activity</a>
     </div>
-    <!-- /.col-lg-12 -->
 </div>
-<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-    <a class="btn btn-primary pull-right" onclick="window.location = '<?php echo site_url('admin/add_activity'); ?>';"> Add  Activity</a>
- </div>
-  <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
-    <div class="table-responsive">
-    <table class="table table-bordered table-hover ">
-   
-                    <tr>
-                        <th>Name</th>
-                        <th>Division</th>
+<div class="row">
+    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover ">
+                <tr>
+                    <th>Name</th>
+                    <th>Division</th>
+                    <th>Product</th>
+                    <th>Action</th>
+                </tr>
+                <tr>
+                    <?php
+                    if (!empty($show)) {
+                        foreach ($show as $row) :
+                            ?><tr>  
+                            <td><?php echo $row->Activity_Name; ?></td>  
+                            <td><?php echo $row->Division; ?>  
+                            <td><?php echo $row->Brand_Name; ?></td>  
 
-                        <th>Product</th>
 
-                      
-                        <th>Action</th>
-                    </tr>
-                    <tr>
-                        <?php 
-                       
-                        if (!empty($show)) {
-                            foreach ($show as $row) :
-                                ?><tr>  
-                                <td><?php echo $row->Activity_Name; ?></td>  
-                                <td><?php echo $row->Division; ?>  
-                                <td><?php echo $row->Brand_Name; ?></td>  
-                                
-                              
-                                <td>  
-                                    <a class="fa fa-trash-o" onclick="window.location = '<?php  echo site_url('admin/act_del?id=') . $row->Activity_id; ?>';"></a> 
-                                    <a class="fa fa-pencil " onclick="window.location = '<?php echo site_url('admin/update_act?id=') . $row->Activity_id; ?>';"></a> </td>
-                                    <?php
-                                endforeach;
-                            }
-                            ?>
-                    </tr>
-                </table>
+                            <td>  
+                                <a class="fa fa-trash-o" onclick="window.location = '<?php echo site_url('admin/act_del?id=') . $row->Activity_id; ?>';"></a> 
+                                <a class="fa fa-pencil " onclick="window.location = '<?php echo site_url('admin/update_act?id=') . $row->Activity_id; ?>';"></a> </td>
+                                <?php
+                            endforeach;
+                        }
+                        ?>
+                </tr>
+            </table>
+        </div>
     </div>
-    </div>
-
-
+</div>
--- application/views/admin/doctor_view.php
+++ application/views/admin/doctor_view.php
@@ -1,52 +1,36 @@
-<style>
-    .table-view .table-view-cell {
-        background-position: 0px 100%;
-    }
-    .col-xs-9, .col-xs-3{
-        padding: 0px;
-    }
-    .table-view-cell {
-        padding: 11px 12px 11px 15px;
-    }
-</style>
-<ul class="table-view ">
-    <li class="table-view-cell table-view-divider"> Doctor List</li>
-    <a class="btn btn-primary pull-right" onclick="window.location = '<?php // echo site_url('admin/emp_add'); ?>';"> Add Doctor</a>
-    <ul class="table-view-cell">
-        <li class="table-view-cell">
-            <table class="table table-borderedb">
-                <tr>
-                    <th>Full_Name</th>
-                    
-
-                    <th>Mobile</th>
-
-                   
-                    <th>City</th> 
-                    <th>Action</th>
-                </tr>
-                <tr>
-                    <?php
-                    if (!empty($show)) {
-                        foreach ($show as $row) :
-                            ?><tr>  
-                            <td><?php echo $row->Account_Name; ?></td>  
-                           
-                            <td><?php echo $row->Mobile; ?></td>  
-                            <td><?php echo $row->City; ?></td>
-                         
-                            <td>  
-                                <a class="fa fa-trash-o" onclick="window.location = '<?php // echo site_url('admin/emp_del?id=') . $row->VEEVA_Employee_ID; ?>';"></a> 
-                                <a class="fa fa-pencil " onclick="window.location = '<?php // echo site_url('admin/update_emp?id=') . $row->VEEVA_Employee_ID; ?>';"></a> </td>
-                                <?php
-                            endforeach;
-                        }
-                        ?>
-                </tr>
-            </table>
-        </li>
-    </ul>
+<div class="row">
+    <div class="col-lg-12">
+        <button class="btn btn-primary">Add Doctor</button>
+    </div>
 </div>
+<div class="row">
+    <div class="col-lg-12">
+        <table class="table table-bordered">
+            <tr>
+                <th>Full_Name</th> 
+                <th>Mobile</th>
+                <th>City</th> 
+                <th>Action</th>
+            </tr>
+            <tr>
+                <?php
+                if (!empty($show)) {
+                    foreach ($show as $row) :
+                        ?><tr>  
+                        <td><?php echo $row->Account_Name; ?></td>  
 
+                        <td><?php echo $row->Mobile; ?></td>  
+                        <td><?php echo $row->City; ?></td>
 
+                        <td>  
+                            <a class="fa fa-trash-o" href ="<?php // echo site_url('admin/emp_del?id=') . $row->VEEVA_Employee_ID;   ?>"></a> 
+                            <a class="fa fa-pencil " href="<?php // echo site_url('admin/update_emp?id=') . $row->VEEVA_Employee_ID;   ?>"></a> </td>
+                            <?php
+                        endforeach;
+                    }
+                    ?>
+            </tr>
+        </table>
+    </div>
+</div>
 
