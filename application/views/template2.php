f7794a375459d21cf55b7ab485cc13c50cdb8f6d
raiyan2723 <raiyanshaikh1993@gmail.com>
2016-01-21 11:22:45 +0530

    hgihgadf

--- application/views/template2.php
+++ application/views/template2.php
@@ -6,13 +6,16 @@
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
-        <link href="<?php echo asset_url() ?>css/ratchet.css" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo asset_url() ?>css/bootstrap.min.css" rel="stylesheet" type="text/css " >
-        <link href="<?php echo asset_url() ?>css/ratchet-theme-ios.min.css" rel="stylesheet" type="text/css"/>
+<!--        <link href="<?php echo asset_url() ?>css/ratchet.css" rel="stylesheet" type="text/css"/>-->
+<!--        <link href="<?php echo asset_url() ?>css/bootstrap.min.css" rel="stylesheet" type="text/css " >-->
+<!--        <link href="<?php echo asset_url() ?>css/ratchet-theme-ios.min.css" rel="stylesheet" type="text/css"/>-->
         <link href="<?php echo asset_url() ?>font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-        <script src="<?php echo asset_url() ?>js/jquery.js" type="text/javascript"></script>
-        <script src="<?php echo asset_url() ?>js/bootstrap.min_1.js" type="text/javascript"></script>
+<!--        <script src="<?php echo asset_url() ?>js/jquery.js" type="text/javascript"></script>-->
+<!--        <script src="<?php echo asset_url() ?>js/bootstrap.min_1.js" type="text/javascript"></script>-->
         <script src='<?php echo asset_url() ?>js/jquery.bootstrap-growl.min.js' type='text/javascript'></script>
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
@@ -100,44 +103,82 @@
         <?php } ?>
     </head>
     <body>
-        <header class="bar bar-nav">
-            <h1 class="title" >  
-                <img style="height: 99%" onclick="window.location = '<?php echo $this->Designation == 'BDM' ? site_url('User/dashboard') : site_url('ASM/dashboard'); ?>';" src="<?php echo asset_url() ?>images/travels.png" alt=""/>
-                <ul class="nav navbar-right top-nav pull-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i><?php echo isset($this->Full_Name) ? ' ' . $this->Full_Name : ''; ?><b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a onclick="window.location = '<?php echo site_url('User/BDM_update'); ?>';"><i class="fa fa-fw fa-user"></i> Profile</a>
-                            </li>
 
-                            <li><a href="#" onclick="window.location = '<?php echo site_url('User/logout'); ?>';"  ><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <!--                
 
-<a class="fa fa-2x fa-power-off pull-right" onclick="window.location = '<?php echo site_url('User/logout'); ?>';" style="padding:8px 0px 0px 0px"></a>-->
-                <?php if ($title != 'Main') { ?>
-                    <a class="fa fa-2x fa-arrow-left pull-left" onclick="window.location = '<?php echo isset($backUrl) ? site_url($backUrl) : site_url('User/dashboard'); ?>';" style="padding:8px 0px 0px 0px"></a>
-                <?php }
-                ?>
+        <!DOCTYPE html>
+    <html lang="en">
+
+        <title>Bootstrap Case</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <img style="" onclick="window.location = '<?php echo $this->Designation == 'BDM' ? site_url('User/dashboard') : site_url('ASM/dashboard'); ?>';" src="<?php echo asset_url() ?>images/travels.png" alt=""/>
+             
+                        <a class="fa fa-2x fa-arrow-left pull-left" onclick="window.location = '<?php echo isset($backUrl) ? site_url($backUrl) : site_url('User/dashboard'); ?>';" style="padding:8px 0px 0px 0px"></a>
+                   
                 </div>
-            </h1>
+                <div class="dropdown pull-right">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i><?php echo isset($this->Full_Name) ? ' ' . $this->Full_Name : ''; ?><b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li> <a onclick="window.location = '<?php echo site_url('User/BDM_update'); ?>';"><i class="fa fa-fw fa-user"></i> Profile</a></li>
+                        <li><a href="#" onclick="window.location = '<?php echo site_url('User/logout'); ?>';"  ><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
+                    </ul>
+                </div>
+            </div>
 
-        </header>
-        <script>
-            function goback() {
+        </nav>
 
-                window.history.back();
-            }
-        </script>
-        <div class="content" style="padding-right: 0px;padding-left: 0px">
+    </html>
 
-            <?php $this->load->view($content, $view_data); ?>
-        </div>
 
-    </body>
-    <!--    <a  href="http://www.jqueryajaxphp.com/dynamically-animate-jquery-knob">Tutorial: Dynamically animate jQuery knob</a>-->
+
+
+
+
+
+    <!--            <h1 class="title" >  
+                    <img style="height: 99%" onclick="window.location = '<?php echo $this->Designation == 'BDM' ? site_url('User/dashboard') : site_url('ASM/dashboard'); ?>';" src="<?php echo asset_url() ?>images/travels.png" alt=""/>
+                    <ul class="nav navbar-right top-nav pull-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i><?php echo isset($this->Full_Name) ? ' ' . $this->Full_Name : ''; ?><b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a onclick="window.location = '<?php echo site_url('User/BDM_update'); ?>';"><i class="fa fa-fw fa-user"></i> Profile</a>
+                                </li>
+    
+                                <li><a href="#" onclick="window.location = '<?php echo site_url('User/logout'); ?>';"  ><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                                    
+    
+    <a class="fa fa-2x fa-power-off pull-right" onclick="window.location = '<?php echo site_url('User/logout'); ?>';" style="padding:8px 0px 0px 0px"></a>
+    <?php if ($title != 'Main') { ?>
+                                                                                            <a class="fa fa-2x fa-arrow-left pull-left" onclick="window.location = '<?php echo isset($backUrl) ? site_url($backUrl) : site_url('User/dashboard'); ?>';" style="padding:8px 0px 0px 0px"></a>
+    <?php }
+    ?>
+                    </div>
+                </h1>-->
+
+
+    <script>
+        function goback() {
+
+            window.history.back();
+        }
+    </script>
+    <div class="content" style="padding-right: 0px;padding-left: 0px">
+
+        <?php $this->load->view($content, $view_data); ?>
+    </div>
+
+</body>
+<!--    <a  href="http://www.jqueryajaxphp.com/dynamically-animate-jquery-knob">Tutorial: Dynamically animate jQuery knob</a>-->
 </html>
 
