bdb5cb92b25938234fa0b233c82c914026ebd673
raiyan2723 <raiyanshaikh1993@gmail.com>
2016-01-22 11:35:02 +0530

    design changes in ASm

--- application/views/Asm/reporting_rx.php
+++ application/views/Asm/reporting_rx.php
@@ -1,6 +1,6 @@
 <div class="panel panel-default">
-    <div class="panel-body">
-            <div class="col-sm-4"></div>
+    <div class="panel-body col-lg-12 col-sm-12 col-md-12 col-xs-12">
+            <div class="col-sm-2"></div>
 
             <?php echo form_open('ASM/reporting_rx'); ?>
             <div class="col-sm-3"  >
