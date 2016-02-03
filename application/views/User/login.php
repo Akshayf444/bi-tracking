a799825b575104eaaba0b5c7a4c763f16f734ab9
sadhnakaushik <sadhnasharma34@gmail.com>
2016-02-03 18:15:54 +0530

    add

--- application/views/User/login.php
+++ application/views/User/login.php
@@ -1,4 +1,11 @@
+<link href="<?php echo asset_url() ?>css/jquery-ui.css" rel="stylesheet" type="text/css " >
+<script src="<?php echo asset_url() ?>js/jquery.js" type="text/javascript"></script>
+<script src="<?php echo asset_url() ?>js/jquery-ui.js" type="text/javascript"></script>
+
+
+
 <div class="container" style="margin-top: 2em;">
+    <div class="pull-right" id="mini-notification">Helpline No : <span class="helpline" style = 'color:red;font-weight:bold'>022-65657701</span><br>From 10 am - 6 pm</div>
     <?php
     if (isset($message)) {
         echo '<div class="alert alert-danger" style="text-align:center">' . $message . '</div>';
@@ -30,34 +37,17 @@
         <div class="col-lg-9">
             <a href="<?php echo site_url('User/forget_pass') ?>" width="45%" class="pull-right" > Forget Password</a>
         </div>
-        </div>
     </div>
-    
-<div class="col-lg-4">
-<button type="button" class="btn btn-primary pull-right " data-toggle="modal" data-target="#myModal">
-  Help
-</button>
-      </div>
-
-
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-         
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-         
-        <h4 class="modal-title" id="myModalLabel">Help</h4>
-      </div>
-      <div class="modal-body">
-          
-          <p  style="text-align: center";> For Any Query Regarding The Reporting,Please Contact<b> 022-65657701</b></p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-     
-      </div>
-    </div>
-  </div>
 </div>
+
+<script>
+    $(function () {
+
+        function blinker() {
+            $('.helpline').fadeOut(500);
+            $('.helpline').fadeIn(500);
+        }
+
+        setInterval(blinker, 1000);
+    });
+</script>
