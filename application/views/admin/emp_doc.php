fbe3b21d05aa30c36da53c92e8553abf641f147c
sadhnakaushik <sadhnasharma34@gmail.com>
2016-02-05 13:08:46 +0530

    addd

--- application/views/admin/emp_doc.php
+++ application/views/admin/emp_doc.php
@@ -1,25 +1,28 @@
 
 <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
     <div class="table-responsive">
-    <table class="table table-bordered table-hover ">
-      <tr>
-      <tr style="background-color: #428BCA">
-                    <th>Full Name</th>
-                    
-                    <th>Specialty</th>
-                </tr>
-                <tr>
-                    <?php
-                    if (!empty($show)) {
-                        foreach ($show as $row) :
-                            ?><tr>  
-                            <td><?php echo $row->Name; ?></td>  
-                         <td><?php echo $row->Specialty; ?></td>  
-                                <?php
-                            endforeach;
-                        }
-                        ?>
-                </tr>
+        <table class="table table-bordered table-hover ">
+            <tr>
+            <tr style="background-color: #428BCA">
+                <th>S.No</th>
+                <th>Full Name</th>
+
+                <th>Specialty</th>
+            </tr>
+            <tr>
+                <?php
+                $counter = 0;
+                if (!empty($show)) {
+                    foreach ($show as $row) :
+                        ?><tr>  
+                        <td><?php echo ++$counter; ?></td>
+                        <td><?php echo $row->Name; ?></td>  
+                        <td><?php echo $row->Specialty; ?></td>  
+                        <?php
+                    endforeach;
+                }
+                ?>
+            </tr>
 
 
 
