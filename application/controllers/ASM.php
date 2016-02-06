55eda3e6eecd470a9b75ee6e9bda9a5a75dbb531
Akshayf444 <mohite.akshay118@gmail.com>
2016-02-06 10:44:07 +0530

    Reomved index function from ASM

--- application/controllers/ASM.php
+++ application/controllers/ASM.php
@@ -31,56 +31,6 @@ class ASM extends MY_Controller {
         }
     }
 
-    public function index() {
-        $data = array();
-        if ($this->input->post()) {
-
-            $username = $this->input->post('username');
-            $password = $this->input->post('password');
-            $check = $this->User_model->authentication($username, $password);
-            if (empty($check)) {
-                $data['message'] = ' Username/password Incorrect';
-                $emp = $this->User_model->employee_id($username);
-                $add = array(
-                    'VEEVA_Employee_ID' => $emp['VEEVA_Employee_ID'],
-                    'password' => $password,
-                    'created_at' => date('Y-m-d H:i:s'),
-                );
-                $this->User_model->password_save($add);
-                $count = $this->User_model->password_count($emp['VEEVA_Employee_ID']);
-                if ($count['cnt'] > 5) {
-                    $data1 = array(
-                        'Status' => 'locked',
-                    );
-                    $this->User_model->update_status($username, $data1);
-                    $data['message'] = 'Your Account Has Been Locked';
-                }
-                $data = array('title' => 'Login', 'content' => 'ASM/login', 'view_data' => $data);
-                $this->load->view('template1', $data);
-            } else {
-                $this->session->set_userdata('VEEVA_Employee_ID', $check['VEEVA_Employee_ID']);
-                $this->session->set_userdata('Local_Employee_ID', $check['Local_Employee_ID']);
-                $this->session->set_userdata('Full_Name', $check['Full_Name']);
-                $this->session->set_userdata('Division', $check['Division']);
-                $this->session->set_userdata('Designation', $check['Designation']);
-                $this->session->set_userdata('Reporting_To', $check['Reporting_To']);
-                $this->session->set_userdata('Reporting_VEEVA_ID', $check['Reporting_VEEVA_ID']);
-                $this->session->set_userdata('Reporting_Local_ID', $check['Reporting_Local_ID']);
-                $this->session->set_userdata('Reporting_To', $check['Reporting_To']);
-
-                $check_password = $this->User_model->password_status($this->session->userdata('VEEVA_Employee_ID'));
-
-                if (is_null($check_password['password_status'])) {
-                    redirect('ASM/dashboard', 'refresh');
-                } else {
-                    redirect('ASM/dashboard', 'refresh');
-                }
-            }
-        }
-        $data = array('title' => 'Login', 'content' => 'ASM/login', 'view_data' => $data);
-        $this->load->view('template1', $data);
-    }
-
     public function dashboard() {
         if ($this->is_logged_in()) {
 
@@ -302,7 +252,6 @@ class ASM extends MY_Controller {
             );
 
             $this->db->insert('Asm_Comment', $comment);
-
 
             $this->session->set_userdata('message', $this->Master_Model->DisplayAlert($approveCount . ' Records  Approved And ' . $rejectCount . ' Records  Rejected', 'success'));
 
